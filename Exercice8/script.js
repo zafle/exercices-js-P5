08f48a11ddce87a3cd3b5774d7cde1b8ddb00587
zafle <fleutotanne@free.fr>
2024-05-17 09:36:47 +0200

    Exercice 8

--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,10 @@
 //Votre code ici
-
+function add(a, b) {
+    const one = parseFloat(a);
+    const two = parseFloat(b);
+    const c = parseFloat(one + two);
+    return c;
+}
+const result = add(3, 8);
+console.log(result);
  export default add;
