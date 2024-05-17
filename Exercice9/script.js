bf6bf3bc6ffc1650d2c3cb42d983cfe2e5758470
zafle <fleutotanne@free.fr>
2024-05-17 10:10:42 +0200

    Exercice 9

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -1,13 +1,12 @@
 // votre code ici
 function checkAge(number) {
     const age = parseInt(number);
-    const message = "";
     if (age < 18) {
-        message = "Vous êtes mineur.";
+       const message = "Vous êtes mineur.";
     } else if (age >= 18 && age <= 65) {
-        message = "Vous êtes majeur.";
+       const message = "Vous êtes majeur.";
     } else if (age > 65) {
-        message = "Vous êtes senior.";
+       const message = "Vous êtes senior.";
     }
     return message;
 }
