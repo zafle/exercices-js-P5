e768ef268e005a89f42f2b031929fab55386241f
zafle <fleutotanne@free.fr>
2024-05-17 10:17:07 +0200

    Exercice 9 equality 65 modified

--- Exercice9/script.js
+++ Exercice9/script.js
@@ -4,9 +4,9 @@ function checkAge(number) {
     let message = "";
     if (age < 18) {
        message = "Vous êtes mineur.";
-    } else if (age >= 18 && age <= 65) {
+    } else if (age >= 18 && age < 65) {
        message = "Vous êtes majeur.";
-    } else if (age > 65) {
+    } else if (age >= 65) {
        message = "Vous êtes senior.";
     }
     return message;
